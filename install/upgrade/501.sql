45a6f18f6024b4d65c8571879f66da7248adc273
frozenice2525 <frozenice2525@fa0464e5-b9b3-beac-175e-12ffb576073b>
2010-07-12 19:13:59 +0000

    aa

--- install/upgrade/501.sql
+++ install/upgrade/501.sql
@@ -4,3 +4,6 @@ UPDATE `%prefix%material_static` SET `material_volume` = '0.1' WHERE `%prefix%ma
 UPDATE `%prefix%material_static` SET `material_volume` = '0.01' WHERE `%prefix%material_static`.`material_id` =16670 LIMIT 1 ;
 UPDATE `%prefix%material_static` SET `material_volume` = '0.01' WHERE `%prefix%material_static`.`material_id` =16673 LIMIT 1 ;
 UPDATE `%prefix%material_static` SET `material_volume` = '0.01' WHERE `%prefix%material_static`.`material_id` =16671 LIMIT 1 ;
+UPDATE `%prefix%material_static` SET `material_volume` = '0.05' WHERE `%prefix%material_static`.`material_id` =16678 LIMIT 1 ;
+UPDATE `%prefix%material_static` SET `material_volume` = '0.1' WHERE `%prefix%material_static`.`material_id` =16635 LIMIT 1 ;
+UPDATE `%prefix%material_static` SET `material_volume` = '0.1' WHERE `%prefix%material_static`.`material_id` =16636 LIMIT 1 ;
