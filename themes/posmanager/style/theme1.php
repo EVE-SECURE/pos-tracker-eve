6979d58db08399a4b323e132d98ed7d157dbb275
frozenice2525 <frozenice2525@fa0464e5-b9b3-beac-175e-12ffb576073b>
2010-08-12 02:51:55 +0000

    5.0.2

---
+++ themes/posmanager/style/theme1.php
@@ -0,0 +1,24 @@
+<?php
+/*Theme 1 = FGV*/
+$focus_fuel          				        = 72;         # Fuel level that trigger a "pre" warning color.
+$minimal_fuel                			    = 48;         # Minimal fuel that should be kept in the towers at any time
+$critical_fuel                              = 24;         # Critical fuel level
+
+$colors['focus_fuel_font_color']          = "#6633FF" ; #
+$colors['minimal_fuel_font_color']        = "#FFFF33" ; #
+$colors['critical_fuel_text_color']       = "#EE0000" ; #
+$colors['owner_text_color']               = "#FFFFFF" ; #
+$colors['edited_text_color']              = "#FFFFFF" ; #
+
+
+$colors['focus_fuel_background_color']    = "#DDDDDD" ; #
+$colors['minimal_fuel_background_color']  = "#BBBBBB" ; #
+$colors['critical_fuel_background_color'] = "#FF9999" ; #
+$colors['owner_background_color']         = "#0099FF" ; #
+$colors['edited_background_color']        = "#007767" ; #
+
+
+$colors['secret_background_color']        = "#72553F" ; #
+$colors['secret_text_color']              = "#FFFFFF" ;
+
+?>
---
+++ themes/posmanager/style/theme3.php
@@ -0,0 +1,24 @@
+<?php
+/*Theme 3 = Majesta Empire*/
+$focus_fuel          				        = 72;         # Fuel level that trigger a "pre" warning color.
+$minimal_fuel                			    = 48;         # Minimal fuel that should be kept in the towers at any time
+$critical_fuel                              = 24;         # Critical fuel level
+
+$colors['focus_fuel_font_color']          = "#ea7d00" ; #
+$colors['minimal_fuel_font_color']        = "#ffd52e" ; #
+$colors['critical_fuel_text_color']       = "#ff0000" ; #
+$colors['owner_text_color']               = "#d5e9ae" ; #
+$colors['edited_text_color']              = "#FFFFFF" ; #
+
+
+$colors['focus_fuel_background_color']    = "#425c60" ; #
+$colors['minimal_fuel_background_color']  = "#515d74" ; #
+$colors['critical_fuel_background_color'] = "#931216" ; #
+$colors['owner_background_color']         = "#20251d" ; #
+$colors['edited_background_color']        = "#007767" ; #
+
+
+$colors['secret_background_color']        = "#72553F" ; #
+$colors['secret_text_color']              = "#FFFFFF" ;
+
+?>
