949c3fff32c4330a9091262566cac33670b8113d
frozenice2525 <frozenice2525@fa0464e5-b9b3-beac-175e-12ffb576073b>
2011-02-13 16:30:33 +0000

    5.1.1 Beta 1

---
+++ trunk/help.php
@@ -0,0 +1,25 @@
+<?php
+include_once 'eveconfig/config.php';
+include_once 'includes/dbfunctions.php';
+
+EveDBInit();
+
+include_once 'includes/eveclass.php';
+include_once 'includes/class.pos.php';
+include_once 'includes/eveRender.class.php';
+
+$eveRender = New eveRender($config, $mod, false);
+$colors    = $eveRender->themeconfig;
+
+$eve     = New Eve();
+$posmgmt = New POSMGMT();
+$userinfo = $posmgmt->GetUserInfo();
+$theme_id = $eve->SessionGetVar('theme_id');
+$access = $eve->SessionGetVar('access');
+$access = explode('.',$access);
+$eveRender->Assign('theme_id', $theme_id);
+$eveRender->Assign('access', $access);
+$eveRender->Assign('config', $config);
+$eveRender->Assign('version', VERSION);
+$eveRender->Display('help.tpl');
+?>
